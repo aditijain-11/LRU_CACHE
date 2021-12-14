c2a3b2ee1ed75a054a58813a4b4a5010900bfc15
ADITI JAIN <aditijain1100@gmail.com>
2021-12-15 04:35:25 +0530

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
- // case 2:
-    //     cout<<"\nEnter capacity of you cache\n";
-    //     int capcache;
-    //     cin>>capcache;
-    //     cout<<"Enter name of you cache";
-    //     string name;
-    //     cin>>name;
-    //     LRUCache * name = new LRUCache(capcache);
